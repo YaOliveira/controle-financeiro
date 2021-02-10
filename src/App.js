e88328f4f48657aa0c9a2089bd10c89828f7f459
Yara Oliveira <yara.oliveira@loggi.com>
2021-02-09 22:08:05 -0300

    feat(form): create select

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './index.css'
-import ReactDom from 'react-dom'
 import React from 'react'
 import Input from './components/Form/input';
+import Select from './components/Form/Select';
 
 const App = () => {
     const [commodity_name, setName] = React.useState('');
     const [commodity_value, setValue] = React.useState('');
+    const [action, setAction] = React.useState('');
 
     return(
         <form>
+            <Select options= {['Compra', 'Venda']}
+                value={action}
+                setValue={setAction}
+            />
             <Input id="commodity_name" label="Nome da Mercadoria" value={commodity_name} setValue={setName}/>
             <Input id="commodity_value" label="Valor" value={commodity_value} setValue={setValue}/>
+            <button>Adicionar transação</button>
         </form>
     )
 }
--- src/components/Form/input.jsx
+++ src/components/Form/input.jsx
@@ -4,7 +4,13 @@ const Input = ({id, label, value, setValue}) => {
     return (
         <div>
             <label htmlFor={id}>{label}</label>
-            <input type="text" name={id} id={id} value={value} onChange={({target}) => setValue(target.value)}/>
+            <input
+             type="text"
+             name={id}
+             id={id}
+             value={value}
+             onChange={({target}) => setValue(target.value)}
+             />
         </div>
     )
 }
