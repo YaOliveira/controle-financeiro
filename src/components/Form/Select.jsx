fe333276ab1d773e3fef89ebdbf5cdd16c9a6fdd
Yara Oliveira <yara.oliveira@loggi.com>
2021-02-10 00:08:02 -0300

    feat(config): add .vscode with settings

--- src/components/Form/Select.jsx
+++ src/components/Form/Select.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Select = ({ options, value, setValue }) => {
+const Select = ({ options, value, setValue, ...props }) => {
 
     return (
         <select 
             value={value}
             onChange={({target}) => setValue(target.value)}
+             {...props}
         >
             <option value="" disabled>
                 Selecione
